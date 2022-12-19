6cec92b81512ecfca914ffd4590869d0af868243
Kishandixit <kishandixit2000@gmail.com>
2022-12-19 13:54:11 +0700

    Updates

---
+++ precache-manifest.8c74b16d42016cf1cd59c27d6c0cd4e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfce23066e0d4076ae39",
+    "url": "/incre-decre-number/static/css/main.bfe1be4f.chunk.css"
+  },
+  {
+    "revision": "bfce23066e0d4076ae39",
+    "url": "/incre-decre-number/static/js/main.bfce2306.chunk.js"
+  },
+  {
+    "revision": "6b045cf713a47c286f3a",
+    "url": "/incre-decre-number/static/css/1.f8137939.chunk.css"
+  },
+  {
+    "revision": "6b045cf713a47c286f3a",
+    "url": "/incre-decre-number/static/js/1.6b045cf7.chunk.js"
+  },
+  {
+    "revision": "7976cc39c60955c25d34",
+    "url": "/incre-decre-number/static/js/runtime~main.7976cc39.js"
+  },
+  {
+    "revision": "2ba9485809782fb596a9dfb27b9017ed",
+    "url": "/incre-decre-number/index.html"
+  }
+];
